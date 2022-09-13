02ee128d2324a43fa0e6c502d2949e69114fffbd
Zatykó Nóra <zatyko.nora@gmail.com>
2022-09-13 22:19:42 +0200

    refactoring index and about pages, routes, controllers

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,18 +6,16 @@ class HomeController extends Controller
     public function index()
     {
         $viewData = [];
-        $viewData["title"] = "Home Page - Online Store";
+        $viewData["title"] = "Home Page - Career Page";
         return view('home.index')->with("viewData", $viewData);
     }
     public function about()
     {
-        $data1 = "About us - Online Store";
-        $data2 = "About us";
-        $description = "This is an about page ...";
-        $author = "Developed by: Your Name";
-        return view('home.about')->with("title", $data1)
-            ->with("subtitle", $data2)
-            ->with("description", $description)
-            ->with("author", $author);
+        $viewData = [];
+        $viewData["title"] = "About us - Company Name";
+        $viewData["subtitle"] = "About us";
+        $viewData["description"] = "This is an about page our company...";
+        $viewData["author"] = "Developed by: Nóra and Sándor";
+        return view('home.about')->with("viewData", $viewData);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    $viewData = [];
-    $viewData["title"] = "Home Page - Online Store";
-    return view('home.index')->with("viewData", $viewData);
-});
-Route::get('/about', 'App \ Http \ Controllers \ HomeController@about')->name("home.about");
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
+Route::get('/about', 'App\Http\Controllers\HomeController@about')->name("home.about");
 
 Route::get('/dashboard', function () {
     return view('dashboard');
