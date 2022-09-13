8d035e9b84e03b7951bd521e70c6555a1a26b21b
Zatykó Nóra <zatyko.nora@gmail.com>
2022-09-13 21:58:30 +0200

    creat index and about pages

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $viewData = [];
+    $viewData["title"] = "Home Page - Online Store";
+    return view('home.index')->with("viewData", $viewData);
 });
+Route::get('/about', 'App \ Http \ Controllers \ HomeController@about')->name("home.about");
 
 Route::get('/dashboard', function () {
     return view('dashboard');
