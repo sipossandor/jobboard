b1b5f30243267cc8271be6e94633f8d49b9c3123
Zatykó Nóra <zatyko.nora@gmail.com>
2022-09-13 21:33:38 +0200

    modify app.blade.php, create the footer section

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
+    <!-- <link href="{{ asset('/css/app.css') }}" rel="stylesheet" /> -->
     <title>@yield('title', 'Career Page')</title>
 </head>
 <body>
@@ -32,6 +33,17 @@
 <div class="container my-4">
     @yield('content')
 </div>
+<!-- footer -->
+<footer id="footer">
+    <div class="copyright py-4 text-center text-black">
+        <div class="container">
+            <small>
+                Copyright - <b>Nóra Zatykó</b> - <b>Sándor Sipos</b>
+            </small>
+        </div>
+    </div>
+</footer>
+<!-- footer -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
 </script>
 </body>
