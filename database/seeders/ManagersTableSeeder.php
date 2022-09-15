62b3fb9e47d5c26f7adfe1a12943f38b21818127
sipossandor <alexsipos13@yahoo.com>
2022-09-15 10:49:29 +0200

    iseed

--- database/seeders/ManagersTableSeeder.php
+++ database/seeders/ManagersTableSeeder.php
@@ -2,18 +2,45 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class ManagersTableSeeder extends Seeder
 {
+
     /**
-     * Run the database seeds.
+     * Auto generated seed file
      *
      * @return void
      */
     public function run()
     {
-        //
+        
+
+        \DB::table('managers')->delete();
+        
+        \DB::table('managers')->insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'name' => 'Arnold Smith',
+                'post' => 'CEO',
+                'photo' => 'link',
+                'created_at' => '2022-09-15 10:24:21',
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ),
+            1 => 
+            array (
+                'id' => 2,
+                'name' => 'Enikő Kiss',
+                'post' => 'HR Manager',
+                'photo' => 'link2',
+                'created_at' => '2022-09-15 10:24:58',
+                'updated_at' => NULL,
+                'deleted_at' => NULL,
+            ),
+        ));
+        
+        
     }
 }
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,18 +2,49 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
 {
+
     /**
-     * Run the database seeds.
+     * Auto generated seed file
      *
      * @return void
      */
     public function run()
     {
-        //
+        
+
+        \DB::table('users')->delete();
+        
+        \DB::table('users')->insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'name' => 'Nóra Zatykó',
+                'email' => 'zatyko.nora@gmail.com',
+                'email_verified_at' => NULL,
+                'password' => '',
+                'role' => 'admin',
+                'remember_token' => NULL,
+                'created_at' => '2022-09-15 10:14:58',
+                'updated_at' => '2022-09-15 10:15:02',
+            ),
+            1 => 
+            array (
+                'id' => 2,
+                'name' => 'Sándor Sipos',
+                'email' => 'sandorsipos13@gmail.com',
+                'email_verified_at' => NULL,
+                'password' => '',
+                'role' => 'admin',
+                'remember_token' => NULL,
+                'created_at' => '2022-09-15 10:16:10',
+                'updated_at' => '2022-09-15 10:16:11',
+            ),
+        ));
+        
+        
     }
 }
