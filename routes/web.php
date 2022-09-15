9c528f4a0325637b78040e490e9dbf156a891dda
sipossandor <alexsipos13@yahoo.com>
2022-09-15 13:50:01 +0200

    create admin panel

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index")
 Route::get('/about', 'App\Http\Controllers\HomeController@about')->name("home.about");
 Route::get('/job_openings', 'App\Http\Controllers\JobOpeningsController@index')->name("job_openings.index");
 Route::get('/job_openings/show/{id}', 'App\Http\Controllers\JobOpeningsController@show')->name("job_openings.show");
+Route::get('/admin', 'App\Http\Controllers\Admin\AdminHomeController@index')->name("admin.home.index");
 
 Route::get('/dashboard', function () {
     return view('dashboard');
